6245d99d571d1cc266190041c52bcd3e2b60cda1
Webert Dias <wdwebert@gmail.com>
2023-03-26 13:07:06 -0300

    Sistema slide funcionando

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-const paisagens = document.querySelector("paisagens");
+const paisagens = document.querySelector(".paisagens");
 
 const imagens = [
-    'inverno.jpeg',
-    'lago.jpeg',
-    'praia1.jpeg'
+    'imagens/lago.jpeg',
+    'imagens/inverno.jpeg',
+    'imagens/praia1.jpeg',
+    'imagens/outono.jpeg'
 ];
 
+setTimeout(slideShow(),2000);
 
-const inicio = setTimeout( slideShow, 2000);
 
-function slideShow (){
+function slideShow(){
 
-    paisagens.src = imagens[i];
-
-    for(const i = 0; i < imagens.length; i++){
-        imagens[i];        
+    for (let i = 0 ; i < imagens.length ; i++) {
+        setTimeout(() =>{
+            paisagens.src = imagens[i];
+        } 
+        ,i *5000);
+        
+     }
     }
-}
+    
 
+
+
+
+//var time = 2000;
+//var count = 0;
+
+//function moveSlideShow(){
+//    paisagens.src = imagens[count];
+
+ //   if(count < imagens.length - 1){
+//          count++;
+//    }else{
+ //       count = 0;
+//    }
+//    setTimeout("moveSlideShow()", time);
+//}
+
+//window.onload = moveSlideShow;
+
+//function slideShow() {
+
+//
